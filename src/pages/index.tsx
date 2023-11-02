22e3a4a762b28faeda9b9db0e9de7b194b83a0d7
Hasan Sufian <hassansofean37@gmail.com>
2023-11-03 01:35:52 +0200

    edit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
@@ -17,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/page/courses">
+            Learn
           </Link>
         </div>
       </div>
@@ -31,10 +30,9 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      description="Learn, Follow, Support">
+      <HomepageHeader/>
       <main>
-        <HomepageFeatures />
       </main>
     </Layout>
   );
